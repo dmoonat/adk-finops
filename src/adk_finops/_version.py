495a6b5fbb01cc3411088c01d6b1bf03d693eab1
dmoonat <moonatdeepak@gmail.com>
2026-09-19 20:57:51 +0530

    chore: bump to v0.4.0

--- src/adk_finops/_version.py
+++ src/adk_finops/_version.py
@@ -19,4 +19,4 @@ try:
     __version__ = _get_version("adk-finops")
 except Exception:
     # Fallback when running from an uninstalled source tree
-    __version__ = "0.3.0"
+    __version__ = "0.4.0"
